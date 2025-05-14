4fd31edcfb1a0780b5f69851b62411d20ae1a457
Alip asmatpasha kamate <92592922+kamatealif@users.noreply.github.com>
2025-05-14 22:02:05 +0530

    Delete src directory

--- src/lib/components/Header.svelte
+++
@@ -1,5 +0,0 @@
-<nav
-	style="padding: 1rem 2rem; display: flex; justify-content: space-between; align-items: center; position: relative; z-index: 2;"
->
-	<h2 style="color: #00ff88;">Alif.dev</h2>
-</nav>
